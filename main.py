d0fa8c18ceeb9b1d0c40f71edfd1c80d42a4f093
Matias <matias@cookunity.com>
2023-01-19 20:33:02 -0300

    cleaned code

--- main.py
+++ main.py
@@ -4,15 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
-
 from login import do_login
-from invoice import do_invoice
 from reports import get_invoiced_amount
-from actions.combobox import select_combo_value
-from actions.button import click_by_class
-from actions.button import click_by_x_path
-from actions.input import write_by_id
 
 from dotenv import load_dotenv
 load_dotenv()
